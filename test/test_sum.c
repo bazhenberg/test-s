b609583a8ce7a59abed3182b30da48ebea0be852
bazhenberg <bazhenov2997@gmail.com>
2016-05-21 11:22:44 +0700

    Update test_sum.c

--- test/test_sum.c
+++ test/test_sum.c
@@ -3,14 +3,14 @@
 
 CTEST(quad_suite, test_quad_equation) 
 {
-	const double a = 1, b = 6, c = 9;
+	const double a = 4, b = 2, c = 6;
 
         double x1, x2;
 
  	quad (a, b, c, &x1, &x2);
  
-	const double expected_x1 = -3;
-	const double expected_x2 = -3;
+	const double expected_x1 = 5;
+	const double expected_x2 = 5;
 	
 	ASSERT_DBL_NEAR(expected_x1, x1);
 	ASSERT_DBL_NEAR(expected_x2, x2);
